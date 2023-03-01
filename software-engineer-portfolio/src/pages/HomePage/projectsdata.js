bacbc147c87bc3a0b03563ac33ac343c6e81ba0a
Constance Nwaigwe <constancenwaigwe@Constances-MacBook-Pro.local>
2023-03-01 12:19:38 -0500

    updated projects

--- software-engineer-portfolio/src/pages/HomePage/projectsdata.js
+++ software-engineer-portfolio/src/pages/HomePage/projectsdata.js
@@ -1,29 +1,30 @@
 import surveyPic from "./images/socialmediasurvey.png";
-import AandLPic from "./images/adviceandlaugh.png";
+// import AandLPic from "./images/adviceandlaugh.png";
+import { calculator } from "./images/calculator.png";
 import resturantPic from "./images/krustykrabui.png";
 import dataPic from "./images/krustykrabbackend.png";
 
 const projectsData = [
-  {
-    image: surveyPic,
-    name: "Social Media Survey",
-    skills: "React CSS JavaScript",
-    uipage: "https://socialmediasurvey.netlify.app/",
-    code: "https://github.com/ConstanceNwaigwe/new-social-media-survey",
-  },
-  {
-    image: AandLPic,
-    name: "Advice and Laugh",
-    skills: "React CSS JavaScript",
-    uipage: "https://github.com/ConstanceNwaigwe/advice-and-laugh",
-    code: "https://github.com/ConstanceNwaigwe/advice-and-laugh",
-  },
   {
     image: resturantPic,
     name: "Krusty Krab Resturant",
     skills: "React CSS JavaScript",
     uipage: "https://krustykrabresturant.netlify.app/",
     code: "https://github.com/Constance-Nwaigwe/spongebob-UI",
+  },
+  {
+    image: calculator,
+    name: "React Calculator",
+    skills: "React CSS JavaScript",
+    uipage: "https://calculatorcreatedwithreact.netlify.app/",
+    code: "https://github.com/ConstanceNwaigwe/web-module-project-reducer",
+  },
+  {
+    image: surveyPic,
+    name: "Social Media Survey",
+    skills: "React CSS JavaScript",
+    uipage: "https://socialmediasurvey.netlify.app/",
+    code: "https://github.com/ConstanceNwaigwe/new-social-media-survey",
   },
   {
     image: dataPic,
